05b6a21cd54fccbafff566a21ebe08427d15767b
Michal <khakaimic4@gmail.com>
2023-05-18 22:13:28 +0300

    completed lab

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,20 @@
-# Your code goes here!
+class Anagram
+    attr_accessor :anagram
+  
+    def initialize(anagram)
+      @anagram = anagram.downcase
+    end
+  
+    def match(words)
+      words.select { |word| anagram?(word.downcase) }
+    end
+  
+    private
+  
+    def anagram?(word)
+      return false if word == anagram || word.length != anagram.length
+  
+      word.chars.sort == anagram.chars.sort
+    end
+  end
+  
